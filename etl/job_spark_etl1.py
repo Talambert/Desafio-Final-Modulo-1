3728001f74f18a42b34e68a73197c6475cc9e3df
Talambert <t.alambert@gmail.com>
2021-08-19 15:52:37 -0300

    arquivos TF

---
+++ etl/job_spark_etl1.py
@@ -0,0 +1,29 @@
+from pyspark.sql.functions import mean, max, min, col, count, lit
+from pyspark.sql import SparkSession
+
+spark = (
+    SparkSession.builder.appName("ExerciceSpark")
+    .getOrCreate()
+)
+
+#leitura
+enem = (
+    spark
+    .read
+    .format("CSV")
+    .option("header", True)
+    .option("inferSchema", True)
+    .option("delimiter", "|")
+    .load("s3://datalake-tancredo-df1-edc-421168935276/raw-data/")
+)
+
+
+#Transformação em parquet
+(
+ enem
+    .write
+    .mode("overwrite")
+    .format("parquet")
+    .partitionBy("NU_ANO_CENSO")
+    .save("s3://datalake-tancredo-df1-edc-421168935276/staging-zone/")
+)
---
+++ etl/upload.py
@@ -0,0 +1,48 @@
+import boto3
+import pandas as pd
+
+#Criar um cliente para interagir com o QWS S3
+s3_client = boto3.client('s3')
+
+s3_client.upload_file("..\dados\microdados_educacao_basica_2020\DADOS\matricula_co.CSV",
+                    "datalake-tancredo-df1-edc-421168935276",
+                    "data/matricula_co.CSV"
+                    )
+s3_client.upload_file("..\dados\microdados_educacao_basica_2020\DADOS\matricula_nordeste.CSV",
+                    "datalake-tancredo-df1-edc-421168935276",
+                    "data/matricula_nordeste.CSV"
+                    )
+
+s3_client.upload_file("..\dados\microdados_educacao_basica_2020\DADOS\matricula_norte.CSV",
+                    "datalake-tancredo-df1-edc-421168935276",
+                    "data/matricula_norte.CSV"
+                    )
+
+s3_client.upload_file("..\dados\microdados_educacao_basica_2020\DADOS\matricula_sudeste.CSV",
+                    "datalake-tancredo-df1-edc-421168935276",
+                    "data/matricula_sudeste.CSV"
+                    )
+
+s3_client.upload_file("..\dados\microdados_educacao_basica_2020\DADOS\matricula_sul.CSV",
+                    "datalake-tancredo-df1-edc-421168935276",
+                    "data/matricula_sul.CSV"
+                    )
+
+
+
+                    
+
+
+
+
+
+
+
+##df = pd.read_csv("..\dados\microdados_educacao_basica_2020\DADOS\gestor.csv", sep="|")
+##print(df)
+
+
+##s3_client.upload_file("..\dados\microdados_educacao_basica_2020\DADOS\*.txt",
+  ##                  "datalake-tancredo-igti-edc",
+    ##                "data/gestor.csv"
+      ##              )
